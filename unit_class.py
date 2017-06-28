7053b983b542c460a3bc2de1057bfebf38bb0e6f
Anhelina Denysenko <kleine.adi@gmail.com>
2017-06-28 18:13:55 +0300

    Add new classes and new to their constructors
    Add new classes: class Warior - child class of Unit class, class Army;
    Write draft __init__ constructor to whole those classes.

--- unit_class.py
+++ unit_class.py
@@ -2,4 +2,29 @@ class Unit:
     """Generic unit class"""
 
     def __init__(self):
-        pass
+        self._hp = 2
+        self._damage = 1
+        self._type = 'Unit'
+
+    def _clash(self, unit):
+    	pass
+
+
+
+class Warior(Unit):
+	"""Child class of Unit class
+	unit warior"""
+
+	def __init__(self, arg):
+		super().__init__()
+		self.arg = arg
+
+		
+
+class Army:
+	"""Generic army class"""
+
+	def __init__(self, _max_unit_in_a_row=3, _max_row=3):
+		self._army = []
+		self._max_unit_in_a_row = _max_unit_in_a_row
+		self._max_row = _max_row
