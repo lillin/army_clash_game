d2e59c14156d9e190853090e5f628bb27ba6509a
Anhelina Denysenko <kleine.adi@gmail.com>
2017-07-03 20:58:41 +0300

    Endured class Army to the Army_class single module Fixed PEP8 in file Added all child classes of Unit_class Define clash func and is_alive func

--- unit_class.py
+++ unit_class.py
@@ -1,30 +1,83 @@
+
+
 class Unit:
     """Generic unit class"""
 
-    def __init__(self):
-        self._hp = 2
-        self._damage = 1
+    def __init__(self, hp=2, damage=1):
+        self._hp = hp
+        self._damage = damage
         self._type = 'Unit'
+        self._status = True
 
-    def _clash(self, unit):
-    	pass
+    def is_alive(self, unit):
+        # check unit status
+        if unit._hp == 0:
+            unit._status = False
+            print('Your unit is dead')
+        else:
+            print('Your unit is alive')
+
+    def clash(self, unit):
+        unit._hp -= self._damage
+        self.is_alive(unit)
 
 
+class Warrior(Unit):
+    """Child class of Unit class
+    unit Warrior
+    """
 
-class Warior(Unit):
-	"""Child class of Unit class
-	unit warior"""
+    def __init__(self):
+        super().__init__()
+        self._type = 'Warrior'
 
-	def __init__(self, arg):
-		super().__init__()
-		self.arg = arg
+    def clash(self, unit):
+        if unit._type == 'Archer':
+            # double damage
+            unit._hp -= self._damage * 2
+        else:
+            unit._hp -= self._damage
+        self.is_alive(unit)
 
-		
 
-class Army:
-	"""Generic army class"""
+class Archer(Unit):
+    """Child class of Unit class
+    unit Archer
+    """
 
-	def __init__(self, _max_unit_in_a_row=3, _max_row=3):
-		self._army = []
-		self._max_unit_in_a_row = _max_unit_in_a_row
-		self._max_row = _max_row
+    def __init__(self):
+        super().__init__()
+        self._type = 'Archer'
+
+    def clash(self, unit):
+        if unit._type == 'Mage':
+            # double damage
+            unit._hp -= self._damage * 2
+        else:
+            unit._hp -= self._damage
+        self.is_alive(unit)
+
+
+class Mage(Unit):
+    """Child class of Unit class
+    unit Mage
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._type = 'Mage'
+
+    def clash(self, unit):
+        if unit._type == 'Warrior':
+            # double damage
+            unit._hp -= self._damage * 2
+        else:
+            unit._hp -= self._damage
+        self.is_alive(unit)
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
